1a5dfd84f52d9364d956d80b965d9fbf4ace73b4
Ranhiru Cooray <ranhiru@gmail.com>
2017-10-28 17:10:36 +0800

    WIP - Added login page with controls

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,30 @@
+import { FormControl, FormGroup } from '@angular/forms';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { LoadingController,
+         NavController,
+         NavParams 
+       } from 'ionic-angular';
 
 @Component({
   selector: 'login-page',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  loginData: FormGroup;
 
+  constructor(public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+   ) {
+    this.loginData = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+   });
+
+  }
+
+  login() {
+    var emailAddress = this.loginData.get("email").value
+    var password = this.loginData.get("password").value
+    console.log("logging in using " + emailAddress + " and " + password);
+  }
 }
